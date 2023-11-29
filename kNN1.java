391cb194649d6e24edf9d686577744b8c2d428b4
starbachi <eelik1916@outlook.com>
2023-11-29 17:36:34 +0000

    Input reading DONE

--- kNN1.java
+++ kNN1.java
@@ -17,15 +17,28 @@ public class kNN1 {
     public static ArrayList<String> readFile() throws FileNotFoundException {
 
         ArrayList<String> dataArr = new ArrayList<String>();
-        File file = new File("C://Users//erudo//OneDrive - University of Kent//Year 2//Autumn Term//CO5280 - Introduction to Artifical Intelligence//ai-a2//train_data.txt");
-        Scanner fileReader = new Scanner(file);
-
-        while (fileReader.hasNext()) {
-            dataArr.add(fileReader.next());  
-            System.out.println(fileReader.next());  
-        }
-        fileReader.close();
+        File dataFile = new File("train_data.txt");
         
+        Scanner dataReader = new Scanner(dataFile);
+
+        while (dataReader.hasNextLine()) {
+            dataArr.add(dataReader.nextLine());
+        }
+        dataReader.close();
+
+        ArrayList<String> labelArr = new ArrayList<String>();
+        File labelFile = new File("train_label.txt");
+        
+        Scanner labelReader = new Scanner(labelFile);
+
+        while (labelReader.hasNext()) {
+            labelArr.add(labelReader.next());
+            System.out.println(labelReader.next());
+        }
+        labelReader.close();
+
+
+
         return dataArr;
     }
 }
