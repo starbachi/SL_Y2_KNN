7eb44e89392e951076a4fa594981efcb2ec011c7
starbachi <eelik1916@outlook.com>
2023-11-29 18:42:30 +0000

    parseLabel Done

--- kNN1.java
+++ kNN1.java
@@ -8,8 +8,8 @@ public class kNN1 {
 
     public static void main(String[] args) throws FileNotFoundException {
         parseData();
+        parseLabel();
     }
-
 
     /*
      * Parses String data to Float and creates a 2D ArrayList
@@ -31,11 +31,29 @@ public class kNN1 {
         return dataArray;
     }
 
-    ArrayList<Integer> labelArr = new ArrayList<>();
-    File labelFile = new File("train_label.txt");
-    Scanner labelReader = new Scanner(labelFile);while(labelReader.hasNext())
-    {
-        labelArr.add(Integer.parseInt(labelReader.next()));
-    }labelReader.close();
+    /*
+     * Parses String labels to Integer and creates an ArrayList<Integer>
+     * 
+     * @return An ArrayList<Integer> containing labels consisting of 0 and 1
+     */
+    public static ArrayList<Integer> parseLabel() throws FileNotFoundException {
+        ArrayList<Integer> labelArr = new ArrayList<>();
+        File labelFile = new File("train_label.txt");
+        Scanner labelReader = new Scanner(labelFile);
+        while (labelReader.hasNext()) {
+            labelArr.add(Integer.parseInt(labelReader.next()));
+        }
+        labelReader.close();
+        return labelArr;
+    }
 
+
+
+
+
+
+
+
+
+    
 }
