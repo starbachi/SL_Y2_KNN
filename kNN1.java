70dadec3d8b374d7364ef577b20151f2d7e9fdbb
starbachi <eelik1916@outlook.com>
2023-12-03 16:51:33 +0000

    Distance done

--- kNN1.java
+++ kNN1.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -7,21 +9,21 @@ import java.util.Scanner;
 public class kNN1 {
 
     public static void main(String[] args) throws FileNotFoundException {
-        parseData();
-        parseLabel();
+        ArrayList<ArrayList<Float>> trainDataArray = new ArrayList<>(parseData(new File("train_data.txt")));
+        ArrayList<ArrayList<Float>> testDataArray = new ArrayList<>(parseData(new File("test_data.txt")));
+        ArrayList<Integer> trainLabelArray = new ArrayList<>(parseLabel(new File("train_label.txt")));
+        ArrayList<Integer> testLabelArray = new ArrayList<>(parseLabel(new File("test_label.txt")));
+        ArrayList<ArrayList<Double>> euclideanDistances = new ArrayList<>(calculateEuclidean(testDataArray, trainDataArray, "output.txt"));
+        System.out.println(euclideanDistances.toString());
     }
 
     /*
-     * Parses String data to Float and creates a 2D ArrayList
-     * 
-     * @return 2D ArrayList containing data parsed to float
+     * Parses @param file to float and creates a 2D ArrayList of features
      */
-    public static ArrayList<ArrayList<Float>> parseData() throws FileNotFoundException {
+    public static ArrayList<ArrayList<Float>> parseData(File file) throws FileNotFoundException {
 
         ArrayList<ArrayList<Float>> dataArray = new ArrayList<>();
-        File dataFile = new File("train_data.txt");
-        Scanner dataScanner = new Scanner(dataFile);
-
+        Scanner dataScanner = new Scanner(file);
         while (dataScanner.hasNextLine()) {
             dataArray.add(new ArrayList<>(Arrays.asList(
                     Arrays.stream(dataScanner.nextLine().split(" ")).map(Float::valueOf).toArray(Float[]::new))));
@@ -36,10 +38,9 @@ public class kNN1 {
      * 
      * @return An ArrayList<Integer> containing labels consisting of 0 and 1
      */
-    public static ArrayList<Integer> parseLabel() throws FileNotFoundException {
+    public static ArrayList<Integer> parseLabel(File file) throws FileNotFoundException {
         ArrayList<Integer> labelArr = new ArrayList<>();
-        File labelFile = new File("train_label.txt");
-        Scanner labelReader = new Scanner(labelFile);
+        Scanner labelReader = new Scanner(file);
         while (labelReader.hasNext()) {
             labelArr.add(Integer.parseInt(labelReader.next()));
         }
@@ -47,13 +48,21 @@ public class kNN1 {
         return labelArr;
     }
 
+    private static ArrayList<ArrayList<Double>> calculateEuclidean(ArrayList<ArrayList<Float>> testInput, ArrayList<ArrayList<Float>> trainInput, String outputFileName) {
+        ArrayList<ArrayList<Double>> distances = new ArrayList<>();
+            ArrayList<Double> innerArr;
+            for(int i = 0; i < testInput.size(); i++) {
+                innerArr = new ArrayList<>();
+                for(int j = 0; j < trainInput.size(); j++) {
+                    double sum = 0;
+                    for (int k = 0; k < testInput.get(i).size(); k++) {
+                        sum +=  Math.pow(testInput.get(i).get(k) - trainInput.get(j).get(k), 2);
+                    }
+                    innerArr.add(Math.sqrt(sum));
+                }
+                distances.add(innerArr);
+            }
+        return distances;
+    }
 
-
-
-
-
-
-
-
-    
 }
